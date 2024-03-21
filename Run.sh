a6af3cc7ad68f8a60f01c96d9a3462570f5e296d
Raiizer <132910758+Raiizer08@users.noreply.github.com>
2024-03-22 00:01:41 +0100

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 clear
-echo -e "\e[4;31m Panda Hackers !!! \e[0m"
+echo -e "\e[4;31m Raiizer \e[0m"
 echo -e "\e[1;32m Presents \e[0m"
 echo -e "\e[1;34m"
 [ `command -v figlet` ] && figlet -f slant Anon-SMS
@@ -17,7 +17,7 @@ apt install figlet toilet python3 curl ruby -y
 apt install python3-pip
 gem install lolcat
 echo This Tool Is Created By HACK3RY2J
-echo This Script Is Created By Panda Hackers HACK3RY2J >update.pandahackers
+echo This Script Is Created By Raiizer >update.pandahackers
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -30,11 +30,7 @@ echo -e "\e[1;31m"
 figlet -f slant Anon-SMS | lolcat
 echo -e "\e[1;34m Created By \e[1;32m"  #blue color
 toilet -f mono12 -F border PH | lolcat
-echo -e "\e[4;34mThis Tool Is Created By Panda Hackers \e[0m" #underline+blue
-echo -e "\e[1;34mFor Any Queries Mail Us!!!\e[0m"
-echo -e "\e[1;32m        Mail: Hack3ry2j@gmail.com \e[0m" #yellow
-echo -e "\e[4;32mYouTube Page: https://youtube.com/c/PandaHackers/ \e[0m" #green
-echo -e "\e[1;34m    Telegram: https://t.me/ItzAnonSMS \e[0m"
+echo -e "\e[4;34mThis Tool Is Created By Raiizer \e[0m" #underline+blue
 echo " "
 echo -e "\e[4;31mRead Instruction Carefully !!! \e[0m" #red
 echo " "
@@ -43,6 +39,7 @@ echo "Enter 2 To  Track Anon-SMS "  #white
 echo "Enter 3 To  Update "
 echo "Enter 4 To  Check Features "
 echo "Enter 5 To  Exit "
+echo "Enter 6 If You're a Nigga"
 read ch
 if [ $ch -eq 1 ];then
 clear
@@ -94,17 +91,6 @@ echo "  Tracking "
 echo "  Automatic Future Updates"
 echo "  Easy To Use And Embed in Code"
 echo""
-echo -e "\e[1;32m                   Contributors\e[1;33m"
-echo -e "\e[1;33m      [*]  Panda Hackers   \e[1;31m"
-echo "         [-] Mail At: pandahacker127@gmail.com"
-echo -e "\e[1;33m      [*]  Instagram   \e[1;31m"
-echo "         [-] Ping At: http://Instagram.com/"
-echo -e "\e[1;33m      [*]  Telegram   \e[1;31m"
-echo "         [-] Ping At: https://t.me/ItzAnonSMS/"
-echo -e "\e[1;33m      [*]  HACK3RY2J   \e[1;31m"
-echo "         [-] Mail At: HACK3RY2J@gmail.com"
-echo -e "\e[1;33m      [*]  HAOI      \e[1;31m"
-echo "         [-] Ping At: http://Instagram.com/haoi_hackers_Academy_of_India/"
 echo ""
 echo ""
 echo -e "\e[1;31m This is Only For Educational Purposes or To Prank.\e[0m"
@@ -114,10 +100,6 @@ echo -e "\e[1;31m We Are Not Responsible For The Misuse Of The Script. \e[0m"
 echo -e "\e[1;32m Update If Doesn't Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
-echo -e "\e[1;34m For Any Queries Mail Us!!!\e[0m"
-echo -e "\e[1;32m        Mail: HACK3RY2J@gmail.com \e[0m"
-echo -e "\e[1;32m   Instagram: https://Instagram.com/ \e[0m"
-echo -e "\e[4;32mYouTube Page: https://www.youtube.com/c/PandaHackers \e[0m"
 echo "Press Enter To Return To Main Menu"
 read a3
 clear
@@ -126,11 +108,7 @@ clear
 echo -e "\e[1;31m"
 figlet -f slant Anon-SMS | lolcat
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f pagga -F border HACK3RY2J | lolcat
-echo -e "\e[1;34m For Any Queries Mail Us!!!\e[0m"
-echo -e "\e[1;32m        Mail: HACK3RY2J@gmail.com \e[0m"
-echo -e "\e[1;32m    Telegram: https://t.me/ItzAnonSMS \e[0m"
-echo -e "\e[4;32mYouTube Page: https://www.youtube.com/c/PandaHackers/ \e[0m"
+toilet -f pagga -F border Raiizer | lolcat
 echo " "
 exit 0
 else
